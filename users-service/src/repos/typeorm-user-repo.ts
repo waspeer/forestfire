000992b942f9fedb42844c081e0c758f823f1367
Wannes Salomé <mail@wannessalome.nl>
2020-01-11 23:59:25 +0100

    fix: removed faulty try/catch block

--- users-service/src/repos/typeorm-user-repo.ts
+++ users-service/src/repos/typeorm-user-repo.ts
@@ -1,10 +1,22 @@
+/* eslint-disable max-classes-per-file */
+import { BaseEntity } from 'typeorm';
 import IUserRepo from './user-repo';
 import User from '../domain/user';
 import UserEmail from '../domain/user-email';
 import UserMap from '../mappers/user-map';
 
+abstract class IOrmUserEntity extends BaseEntity {
+  id!: string;
+  email!: string;
+  passwordHash!: string;
+}
+
 export default class TypeOrmUserRepo implements IUserRepo {
-  constructor(private activeRecord: any) {}
+  private activeRecord: typeof IOrmUserEntity;
+
+  constructor(activeRecord: typeof IOrmUserEntity) {
+    this.activeRecord = activeRecord;
+  }
 
   public async findUserByEmail(email: UserEmail): Promise<User | null> {
     const ormUserOrUndefined = await this.activeRecord.findOne({
@@ -15,26 +27,24 @@ export default class TypeOrmUserRepo implements IUserRepo {
   }
 
   public async exists(email: UserEmail): Promise<boolean> {
-    const ormUser = await this.activeRecord.findOne({ email: email.value });
+    const ormUser = await this.activeRecord.findOne({
+      email: email.value
+    });
     return !!ormUser;
   }
 
   public async save(user: User): Promise<void> {
     const exists = await this.exists(user.email);
 
-    try {
-      if (!exists) {
-        const ormUser = this.activeRecord.create(UserMap.toPersistence(user));
-        await ormUser.save();
-      } else {
-        const ormUser = await this.activeRecord.findOne({
-          email: user.email.value
-        });
-        this.activeRecord.merge(ormUser!, UserMap.toPersistence(user));
-        ormUser!.save();
-      }
-    } catch (e) {
-      console.error(e);
+    if (!exists) {
+      const ormUser = this.activeRecord.create(UserMap.toPersistence(user));
+      await ormUser.save();
+    } else {
+      const ormUser = (await this.activeRecord.findOne({
+        email: user.email.value
+      })) as IOrmUserEntity;
+      this.activeRecord.merge(ormUser, UserMap.toPersistence(user));
+      await ormUser.save();
     }
   }
 }
